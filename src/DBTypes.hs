e3bf9e92739fd5f778d200c86cee09f423117054
Susan Tu <susanctu@gmail.com>
2014-05-31 21:45:56 -0700

    amended data structures

--- src/DBTypes.hs
+++ src/DBTypes.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE ExistentialQuantification #-} 
 {-# LANGUAGE RankNTypes #-}
 
+import Data.Typeable 
+
 {- Exporting all types, constructors, and accessors -}
 module DBTypes (Tablename(..), ErrString(..), Database(..), Fieldname(..), Table(..), Column(..), Element(..), TransactionID(..), RowHash(..), LogOperation(..)) where
 
@@ -12,22 +14,25 @@ newtype Tablename = Tablename String deriving(Eq, Show)
 newtype ErrString = ErrString String deriving(Show)
 
 data Database = Database { database :: Map Tablename (TVar Table) }
-data Fieldname a = Fieldname { fieldname :: String } deriving(Eq, Show)
+data Fieldname = Fieldname { fieldname :: String } deriving(Eq, Show)
 
-data Table = forall a. (Show a, Ord a) => Table { rowCounter :: Int 
-                                                , primaryKey :: Maybe (Fieldname a) 
-                                                , table :: Map (Fieldname a) (Column a)}
-data Column a = Column { default_val :: Maybe (Element a)
-                       , column :: TVar(Map RowHash (Element a))
-                       } -- first element is default value
+data Table = Table { rowCounter :: Int 
+                   , primaryKey :: Maybe Fieldname 
+                   , table :: Map Fieldname Column}
 
-data Element a = Element { element :: TVar (Maybe a) }
+data Column = Column { default_val :: Maybe Element
+                     , col_type :: TypeRep
+                     , column :: TVar(Map RowHash Element)
+                     } -- first element is default value
+
+data Element = forall a. Element { element :: TVar (Maybe a) }
 
 data TransactionID = TransactionID { clientName :: String 
                                    , transactionNum :: Int 
                                    } deriving(Eq, Show, Read)-- clientname, transaction number
  
- data Row = Row {getField :: Fieldname a -> a}
+data Row = Row {getField :: Fieldname -> Maybe Element}
+
 newtype RowHash = RowHash Int deriving(Show, Read) 
 data LogOperation = Start TransactionID
                   | forall a. (Show a, Ord a) => TransactionLog TransactionID (Tablename, Fieldname a, Rowhash) (Maybe a) (Maybe a) -- last two are old val, new val
