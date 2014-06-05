ea27dd8d22955eb6a60264acdb3a81a95afe7162
Susan Tu <susanctu@gmail.com>
2014-06-05 05:15:46 -0700

    update, written in a silly way (fix this later)

--- src/Operation.hs
+++ src/Operation.hs
@@ -302,9 +302,57 @@ delete db tr_id tablename conds = do
                  writeTVar x Table{rowCounter=(rowCounter t), primaryKey=(primaryKey t), table=new_table_map}
                  return $ Right $ fmap (\(hash, list_of_elems) -> Delete tr_id (tablename, hash) (zip fieldnames list_of_elems)) $ zip delete_hashes list_of_list_of_elems      
     Nothing -> return $ Left $ ErrString (show(tablename) ++ " not found.")
- 
-update :: TVar Database -> TransactionID -> Tablename -> (Row -> STM Bool) -> (Row -> Row) -> STM (Either ErrString LogOperation)
-update db tr_id tablename conds changes = return $ Left $ ErrString "unimplemented!"
+
+{-Private-}
+-- returns the new values as well 
+change_rows_from_column :: Table -> [RowHash] -> (Row -> Row) -> (Fieldname, Column) -> STM((Fieldname, Column), [Element]) 
+change_rows_from_column t hashes changes (fieldname, col) = do
+  col_map <- readTVar $ column col 
+  (new_col_map, new_elems) <- change_rows_from_col_map t hashes changes col_map []
+  tvar_col <- newTVar (new_col_map)
+  return ((fieldname, Column{default_val=(default_val col), col_type=(col_type col),column=tvar_col}), new_elems)
+
+{-Private-}
+update_map :: (Element -> STM(Maybe Element)) -> RowHash -> L.Map RowHash (TVar Element) ->STM(L.Map RowHash (TVar Element), Element)
+update_map func hash col_map = case L.lookup hash col_map of
+  Just tvar_elem -> do old_elem <- readTVar tvar_elem
+                       maybe_new_elem <- func old_elem 
+                       case maybe_new_elem of 
+                         Just new_elem -> do new_tvar_elem <- newTVar new_elem
+                                             return $ (L.insert hash new_tvar_elem col_map, new_elem)  
+                         Nothing -> return $ (col_map, old_elem)
+  Nothing -> return (col_map, Element(Nothing::Maybe Int)) -- should not happen!
+
+{-Private-}
+change_rows_from_col_map :: Table -> [RowHash] -> (Row -> Row) -> L.Map RowHash (TVar Element) -> [Element] -> STM(L.Map RowHash (TVar Element), [Element]) -- could return deleted vals for each hash in that col
+change_rows_from_col_map t (h:hashes) changes col_map new_elems = do 
+  (new_col_map, element_list) <- update_map func h col_map
+  change_rows_from_col_map t hashes changes new_col_map (element_list:new_elems)
+  where func a = do (rowhash, old_row) <- construct_row_for_hash (table t) h
+                    let new_row = changes old_row 
+                    old_elems <- sequence $ fmap (getField old_row) $ get_all_fields t
+                    new_elems <- sequence $ fmap (getField new_row) $ get_all_fields t
+                    case M.find (\(old_e, new_e) -> Just a == old_e) (zip old_elems new_elems) of 
+                      Just (_, new_element) -> return $ new_element 
+                      Nothing -> return Nothing 
+change_rows_from_col_map _ _ _ col_map new_elems = return (col_map, new_elems)
+
+{-Public-}
+update :: TVar Database -> TransactionID -> Tablename -> (Row -> STM Bool) -> (Row -> Row) -> STM (Either ErrString [LogOperation])
+update db tr_id tablename conds changes = do
+  tvar_table <- get_table db tablename
+  case tvar_table of 
+    Just x -> do t <- readTVar x -- t is of type Table
+                 (change_hashes, rows) <- get_rows (table t) conds
+                 let fieldnames = (get_all_fields t)
+                 -- get the old values: 
+                 list_of_list_of_elems <- (sequence $ fmap (\r -> liftM catMaybes (mapM (getField r) fieldnames)) rows)::STM([[Element]])
+                 res <- mapM (change_rows_from_column t change_hashes changes) $ L.toList(table t)
+                 let (list_for_table_map, new_elements) = unzip res
+                 let new_table_map = L.fromList list_for_table_map
+                 writeTVar x Table{rowCounter=(rowCounter t), primaryKey=(primaryKey t), table=new_table_map}
+                 return $ Right $ fmap (\(hash, list_of_old_elems, list_of_new_elems) -> Update tr_id (tablename, hash) (zip3 fieldnames list_of_old_elems list_of_new_elems)) $ zip3 change_hashes list_of_list_of_elems new_elements     
+    Nothing -> return $ Left $ ErrString (show(tablename) ++ " not found.")
 
 {-Public-}
 show_tables :: TVar Database -> STM (String) -- doesn't actually update the db, so no need for logstring
